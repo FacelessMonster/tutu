6aea3bd55e0ea5eabb22911ea1b3aac5e9c3fa40
FacelessMonster <marsthegod@protonmail.com>
2019-07-19 12:36:13 +0300

    upd

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -3,5 +3,13 @@ class Carriage < ApplicationRecord
   belongs_to :train, class_name: "Train", foreign_key: "train_id"
   validates :type, format: { with: /\A(ComfortCarriage|EconomyCarriage)\Z/, message: "only allows letters" }
 
+  before_save :set_number
+
+  scope :economy, -> { where(type: 'EconomyCarriage', seats: 54) }
+  scope :comfort, -> { where(type: 'ComfortCarriage', seats: 36) }
+
+  def set_number
+    self.number = (self.train.carriages.count.to_i) + 1
+  end
 
 end
