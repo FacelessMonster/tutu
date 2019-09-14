6c780065c71aebb4ec9079324fb125fc231c48a7
FacelessMonster <marsthegod@protonmail.com>
2019-09-14 21:10:56 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,14 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://tutu-app.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'tutu-app.herokuapp.com',
-    user_name:            GMAIL_SMTP_USER,
-    password:             GMAIL_SMTP_PASSWORD,
-    authentication:       'plain',
-    enable_starttls_auto: true }
+
 end
