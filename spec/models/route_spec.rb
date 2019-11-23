85bf4400851288af4c83a19205ac20ec5f36adff
FacelessMonster <marsthegod@protonmail.com>
2019-11-23 04:24:17 +0300

    Create route_spec.rb

---
+++ spec/models/route_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe Route, '.set_times' do
+  it "sets time for stations on the route" do
+    route = Route.create(name: "Moscow - Vladivostok")
+    first_station = RailwayStation.create(title: "Moscow")
+    last_station = RailwayStation.create(title: "Vladivostok")
+    route.stations_routes.create(railway_station_id: first_station.id)
+    route.stations_routes.create(railway_station_id: last_station.id)
+    times = { first_station.id => '10:00', last_station.id => '22:00' }
+    Route.set_times(route, times)
+    expect(StationsRoute.where(id: first_station.id).first.time).to eq '10:00'
+  end
+end
